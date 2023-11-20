da9a9bef835bcd47914a55cda354fb754630e5f5
Bronu Tarto <brunotarto@gmail.com>
2023-11-20 10:12:10 +0100

    origin changed

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(express.json());
 
 const corsOptions = {
-  origin: 'http://localhost:4200', // or the specific origin you want to allow
+  origin: ['https://xomble.com', 'https://www.xomble.com'], // or the specific origin you want to allow
   credentials: true, // to allow cookies and headers to be sent along with the request
 };
 
